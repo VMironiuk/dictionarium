d28103bf7857f7a07b3baab1e1294c3bded2ea98
Vova Myroniuk <vladimironiuk@gmail.com>
2017-08-24 15:47:03 +0300

    Add style for login form

---
+++ dictionarium/src/org/dictionarium/servlet/LoginServlet.java
@@ -0,0 +1,31 @@
+package org.dictionarium.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/login"})
+
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		RequestDispatcher dispatcher = request.getServletContext()
+				.getRequestDispatcher("/WEB-INF/views/login.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		this.doGet(request, response);
+	}
+}
