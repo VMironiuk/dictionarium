bb3ce9ab657ccd46e38bdb5aa1b1dea799c63df9
Vova Myroniuk <vladimironiuk@gmail.com>
2017-08-24 15:56:14 +0300

    Update user_tbl.sql

--- user_tbl.sql
+++ user_tbl.sql
@@ -2,6 +2,7 @@ CREATE TABLE user_tbl
 (
   user_name VARCHAR(30) NOT NULL,
   password VARCHAR(30) NOT NULL,
+  email VARCHAR(30) NOT NULL,
   dictionary_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (user_name)
 );
